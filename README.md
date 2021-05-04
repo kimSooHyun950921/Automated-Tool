0b2ea3b967a0b87eb553807ae7337c6597d051de
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-05-04 15:43:21 +0900

    add bl parsing modules

--- README.md
+++ README.md
@@ -13,4 +13,7 @@
     - 파싱 위해 pdfreader 모듈 가져옴
         - 출처 코드: https://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python
     - pdf 파일의 값을 어떻게 가져오냐가 문제
+- 05 04
+    - pdf 파일읽어와서 parsing 완료
+    - re를 이용하여 찾아오는건 한줄인데, 실제로 정답은 한줄이 아닐 수 있다. 다양한 경우를 봐야한다.
 
--- parsing_bl.py
+++ parsing_bl.py
@@ -14,25 +14,67 @@ def convert_pdf_to_txt(path):
     fp = open(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     password = ""
-    maxpages = 0
+    maxpages = 1
     caching = True
     pagenos=set()
 
     for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
         interpreter.process_page(page)
-
     text = retstr.getvalue()
-
     fp.close()
     device.close()
     retstr.close()
     return text
 
 
+def get_VS(text):
+    result = None
+    pattern = re.compile('(VESSEL.*)([\s\S]*)(PORT\WOF\WDISCHARGE.*)', re.MULTILINE)
+    result = pattern.search(text)
+    if result:
+        return result[2].strip().split('\n')
+    return result
+
+
+def get_POD(text):
+    result = None
+    pattern = re.compile('(PORT\WOF\WDISCHARGE.*)\s(.+)')
+    result = pattern.search(text)
+    if result:
+        return result[2].strip().split('\n')
+    return result
+
+
+def get_POL(text):
+    result = None
+    pattern = re.compile('(PORT\WOF\WLOADING.*)\s(.+)')
+    result = pattern.search(text)
+    if result:
+        return result[2].strip().split('\n')
+    return result
+
+
+def get_hc(text):
+    #(H\.S\.CODE\W*:\W*)(\d+\.\d+)
+    result = None
+    pattern = re.compile('(H\.S\.CODE\W*:\W*)(\d+\.\d+)')
+    result = pattern.search(text)
+    if result:
+        return result[2].strip().split('\n')
+    return result
+
+
 
 def main():
     result = convert_pdf_to_txt('./example.pdf')
-    print(result)
+    print(get_VS(result))
+    print(get_POD(result))
+    print(get_POL(result))
+    print(get_hc(result))
+    
+
+    
+    
 
 if __name__ == "__main__":
     main()
